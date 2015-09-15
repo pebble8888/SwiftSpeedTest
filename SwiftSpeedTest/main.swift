657604f91191d128b1fda01869aec98d81109b81
pebble8888 <pebble8888@gmail.com>
2015-09-15 21:15:05 +0900

    Initial Commit

---
+++ SwiftSpeedTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftSpeedTest
+//
+//  Created by pebble8888 on 2015/09/15.
+//  Copyright © 2015年 pebble8888. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
